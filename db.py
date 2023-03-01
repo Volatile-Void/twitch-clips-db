54ec07aabd4440a2bb4b9090efc598fbe962af9d
unknown <96886462+Volatile-Void@users.noreply.github.com>
2023-03-01 22:02:18 +0200

    Add select sql generator.

--- db.py
+++ db.py
@@ -11,6 +11,68 @@ def format_sql_value(value):
         value = value.strftime('%Y-%m-%d')
     return repr(value)
 
+class Comparison:
+    operator = None
+
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def __str__(self):
+        return '%s%s%s' % (self.a, self.operator, format_sql_value(self.b))
+
+class Eq(Comparison):
+    operator = '='
+
+class Ne(Comparison):
+    operator = '!='
+
+class Gt(Comparison):
+    operator = '>'
+
+class Ge(Comparison):
+    operator = '>='
+
+class Lt(Comparison):
+    operator = '<'
+
+class Le(Comparison):
+    operator = '<='
+
+class IsNull(Comparison):
+    operator = ' is '
+
+    def __init__(self, a):
+        self.a = a
+        self.b = None
+
+class IsNotNull(IsNull):
+    operator = ' is not '
+
+class LogicalGroup:
+    def __init__(self, *filters):
+        self.filters = filters
+
+    def __str__(self):
+        return (' %s ' % self.sep).join(s for s in (self._filter_to_str(f) for f in self.filters) if s)
+
+    @staticmethod
+    def _filter_to_str(f):
+        if isinstance(f, Comparison):
+            return str(f)
+        if isinstance(f, LogicalGroup):
+            if len(f.filters) == 0:
+                return ''
+            if len(f.filters) == 1:
+                return str(f.filters[0])
+        return '(%s)' % str(f)
+
+class AndGroup(LogicalGroup):
+    sep = 'and'
+
+class OrGroup(LogicalGroup):
+    sep = 'or'
+
 class Column:
     def __init__(self, name, data_type, constraint=None):
         self.name = name
@@ -53,6 +115,7 @@ class Table:
 
     def __init_subclass__(cls):
         cls.column_list = tuple(c.name for c in cls.columns)
+        cls.column_lookup = {c.name: c for c in cls.columns}
 
     @classmethod
     def create_table_sql(cls, schema=None, if_not_exists=False):
@@ -85,5 +148,26 @@ class Table:
             'table': cls.name,
             'columns': ','.join(cls.column_list),
             'rows': ','.join(cls.create_insert_sql_part(obj) for obj in objects),
+        }
+        return sql % parts
+
+    @classmethod
+    def create_select_sql(cls, columns=None, filter_obj=None, filter_logic=AndGroup, custom_where='', custom_where_logic=AndGroup):
+        if columns is None:
+            columns = cls.columns_list
+        elif not isinstance(columns, (list, tuple)):
+            columns = columns,
+        where = custom_where
+        if filter_obj:
+            generated_where = filter_logic(*(Eq(col, val) for col, val in filter_obj.items() if col in cls.column_lookup))
+            if where:
+                where = custom_where_logic(generated_where, where)
+            else:
+                where = generated_where
+        sql = 'select %(columns)s from %(table)s%(where)s'
+        parts = {
+            'columns': ','.join(columns),
+            'table': cls.name,
+            'where': ' where %s' % where if where else '',
         }
         return sql % parts
